8815a7029e400a1b6a50dabaea346d070f7201db
GitGideon <gitgideon@protonmail.com>
2018-02-16 20:39:03 +0100

    Created the minimal branch

--- src/main/java/io/github/gitgideon/valv/Valv.java
+++ src/main/java/io/github/gitgideon/valv/Valv.java
@@ -1,20 +1,9 @@
 package io.github.gitgideon.valv;
 
-import io.github.gitgideon.valv.api.ValvAPI;
-import io.github.gitgideon.valv.economy.EssentialsEcoEconomy;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Valv extends JavaPlugin {
 
 
-    @Override
-    public void onEnable() {
-        setupEconomies();
-    }
-
-    private void setupEconomies() {
-        if (getServer().getPluginManager().getPlugin("Essentials") != null)
-            ValvAPI.economy = new EssentialsEcoEconomy(this);
-    }
 
 }
