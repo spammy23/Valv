68e835a776a62ab0b858dbf67a43a6c13aad8b45
BuildTools <unconfigured@null.spigotmc.org>
2018-02-16 20:20:51 +0100

    Added a method to support Economy plugins registering themselves

--- src/main/java/io/github/gitgideon/valv/api/ValvAPI.java
+++ src/main/java/io/github/gitgideon/valv/api/ValvAPI.java
@@ -4,4 +4,12 @@ public class ValvAPI {
 
     public static ValvEconomy economy = null;
 
+    /**
+     * Register a ValvEconomy instance
+     * @param econ the ValvEconomy instance you want to register
+     */
+    public static void registerValvEconomy(ValvEconomy econ) {
+        economy = econ;
+    }
+
 }
